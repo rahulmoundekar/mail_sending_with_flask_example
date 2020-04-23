bc369c6e3e7fd2dac801dc89b3f4edec3ce4d6a5
rahul <rahulmoundekar44@gmail.com>
2020-04-23 11:25:30 +0530

    mail_sending_with_flask_example

---
+++ settings.py
@@ -0,0 +1,11 @@
+# configuration
+class Config:
+    DEBUG = True
+
+    # EMAIL SETTINGS
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = 'yourmail@gmail.com'
+    MAIL_PASSWORD = 'app-password'
